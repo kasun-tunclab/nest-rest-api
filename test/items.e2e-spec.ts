e92ae707c4c45b3c0176fee7f69733eb09c3f082
Kasun Dilunika <kasun@tunclab.com>
2021-12-02 22:01:31 +1300

    feat: Adding e2e tests for items API.

---
+++ test/items.e2e-spec.ts
@@ -0,0 +1,27 @@
+import {INestApplication} from "@nestjs/common";
+import {Test, TestingModule} from "@nestjs/testing";
+import * as request from "supertest";
+import {ItemsModule} from "../src/items/items.module";
+
+describe('Items API (e2e)', () => {
+
+    let app: INestApplication;
+
+    beforeEach(async () => {
+
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [ItemsModule]
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        await app.init();
+
+    });
+
+    it('Should get all items',  () => {
+
+        return request(app.getHttpServer())
+            .get('/items')
+            .expect(200);
+    });
+});
