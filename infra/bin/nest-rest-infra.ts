6f7329df7885f0325851e2841eada8597018ee36
Kasun Dilunika <kasun@tunclab.com>
2021-12-19 23:17:27 +1300

    Infra stacks are divided into two - Network and API.

--- infra/bin/nest-rest-infra.ts
+++ infra/bin/nest-rest-infra.ts
@@ -1,13 +1,22 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { NestRestInfraStack } from '../lib/nest-rest-infra-stack';
+import { ApiInfraStack } from '../lib/api-infra-stack';
+import {NetworkInfraStack} from "../lib/network-infra-stack";
 
 const app = new cdk.App();
 
 const dockerImageTag = app.node.tryGetContext('dockerImageTag');
 
-new NestRestInfraStack(app, 'NestRestInfraStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-  dockerImageTag
+const env = { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION };
+
+const networkStack = new NetworkInfraStack(app, 'NetworkInfraStack', {
+  env
+});
+
+new ApiInfraStack(app, 'ApiInfraStack', {
+  env,
+  dockerImageTag,
+  ecrRepo: networkStack.ecrRepo,
+  fargateCluster: networkStack.fargateCluster
 });
