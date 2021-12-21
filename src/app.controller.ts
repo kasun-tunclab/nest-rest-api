059d33b5b173fe805ca7e9b466bbf0115c3b01f4
Kasun Dilunika <kasun@tunclab.com>
2021-12-22 10:20:37 +1300

    Ping controller is added. #minor

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,10 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get("/ping")
+  ping(): string {
     return this.appService.getHello();
   }
+
+  // add health checks using  https://docs.nestjs.com/recipes/terminus
 }
