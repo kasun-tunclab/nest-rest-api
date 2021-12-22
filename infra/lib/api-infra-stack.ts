0cdf5e4aed39ed617d8513e27953dd28d80b1859
Kasun Dilunika <kasun@tunclab.com>
2021-12-22 22:52:51 +1300

    ALB now has https listener linked DNS.

--- infra/lib/api-infra-stack.ts
+++ infra/lib/api-infra-stack.ts
@@ -5,6 +5,9 @@ import * as iam from "aws-cdk-lib/aws-iam";
 import {Construct} from "constructs";
 import * as ecr from "aws-cdk-lib/aws-ecr";
 import * as logs from "aws-cdk-lib/aws-logs";
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as r53Targets from 'aws-cdk-lib/aws-route53-targets';
 
 export interface NestRestInfraStackProps extends core.StackProps{
   fargateCluster: ecs.Cluster;
@@ -18,14 +21,23 @@ export class ApiInfraStack extends core.Stack {
 
     super(scope, id, props);
 
+    const hostedZone = new route53.HostedZone(this, 'HostedZone', {
+      zoneName: 'kasun.clearsigma.com',
+    });
+
+    const apiCert = new acm.Certificate(this, 'Certificate', {
+      domainName: 'api.nest.kasun.clearsigma.com',
+      validation: acm.CertificateValidation.fromDns(hostedZone),
+    });
+
     const taskDefinition = new ecs.FargateTaskDefinition(this, 'ApiTaskDef', {
       family: 'nest-api',
       cpu: 256,
       memoryLimitMiB: 512
     });
 
-    taskDefinition.addContainer('api-image', {
-      containerName: 'api',
+    taskDefinition.addContainer('ApiImage', {
+      containerName: 'nest-api-container',
       image: ecs.ContainerImage.fromEcrRepository(props.ecrRepo, props.dockerImageTag),
       portMappings: [{containerPort: 3000}],
       logging: ecs.LogDriver.awsLogs({
@@ -34,7 +46,7 @@ export class ApiInfraStack extends core.Stack {
       })
     });
 
-    const taskExecutionPolicy = iam.ManagedPolicy.fromManagedPolicyArn(this, 'task-exec-policy', 'arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy')
+    const taskExecutionPolicy = iam.ManagedPolicy.fromManagedPolicyArn(this, 'TaskExecutionPolicy', 'arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy')
     taskDefinition.executionRole?.addManagedPolicy(taskExecutionPolicy);
 
     const service = new pat.ApplicationLoadBalancedFargateService(this, 'ApiService', {
@@ -43,10 +55,22 @@ export class ApiInfraStack extends core.Stack {
       taskDefinition,
       desiredCount: 1,
       assignPublicIp: true,
+      redirectHTTP: true,
+      certificate: apiCert,
+      circuitBreaker: {
+        rollback: true,
+      }
     });
 
     service.targetGroup.configureHealthCheck({
       path: '/v1/ping'
+    });
+
+    const albTarget = new r53Targets.LoadBalancerTarget(service.loadBalancer)
+
+    new route53.ARecord(this, 'AlbARecord', {
+      zone: hostedZone,
+      target: route53.RecordTarget.fromAlias(albTarget)
     });
 
   }
